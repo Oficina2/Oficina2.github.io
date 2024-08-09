63c82ef87f0980e5fa644e41f80363cd3231c67c
Oficina2 <oficina.2024.adm@gmail.com>
2024-08-09 20:37:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const dateElement = document.getElementById('date');
+    const today = new Date();
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    dateElement.textContent = today.toLocaleDateString('es-ES', options);
+});
